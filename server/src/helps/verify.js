56e1e44e77eb54e8f7a50e238cb29efebd9db462
sourabh-969 <sourabhsaroj1@gmail.com>
2025-02-24 17:57:39 +0530

    fixing req for accesstoken issue

--- server/src/helps/verify.js
+++ server/src/helps/verify.js
@@ -1,12 +1,23 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
+  //console.log("verifyToken middleware executed");
   const token = req.cookies.accessToken;
-  if (!token) return res.status(401).json("You are not authenticated.");
+  console.log("Token from cookies:", token);
+  if (!token) {
+    //console.log("No token found in cookies");
+    return res.status(401).json({ error: "Authentication required. Please log in." });
+  }
 
-  jwt.verify(token, process.env.SECRET_JWT, (error, user) => {
-    if (error) return res.status(403).json("Your token is invalid.");
-    req.channel = user;
+  jwt.verify(token, process.env.SECRET_JWT, (error, decoded) => {
+    if (error) {
+      //console.error("Token Verification Error:", error);
+      return res.status(403).json({ error: "Invalid or expired token. Please log in again." });
+    }
+
+    // Attach the decoded channel info. to the req.object
+    //console.log("Decoded Token:", decoded);
+    req.channel = decoded;
     next();
   });
 };
